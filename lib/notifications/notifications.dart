f417b1a20e19b808839fd1b8ce00c9aba6f72928
Julio Olivares <jolivares@forte.io>
2022-05-17 20:49:58 +0700

    imprimiendo el fcm token

--- lib/notifications/notifications.dart
+++ lib/notifications/notifications.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void initNotifications(BuildContext context) async {
+  var fcmToken = await FirebaseMessaging.instance.getToken();
+  print("FCM token: " + (fcmToken ?? "no token"));
+
   FlutterLocalNotificationsPlugin notifications =
       FlutterLocalNotificationsPlugin();
 
